c7f2debd00a8dc9d6c48829560640a00954a23df
KIMMYSJ <38124097+KIMMYSJ@users.noreply.github.com>
2018-04-13 14:58:00 +0900

    Update MainActivity.java

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -14,9 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
 
         textView = findViewById(R.id.textView);
-
+        texView.setText("HOMMIEMIN");
 
 
         
