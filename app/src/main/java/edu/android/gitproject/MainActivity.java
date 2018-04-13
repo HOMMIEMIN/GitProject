af8a3fdee8400bf367468a9c2316b147cca9d688
HOMMIEMIN <38124131+HOMMIEMIN@users.noreply.github.com>
2018-04-13 13:40:58 +0900

    Update MainActivity.java

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -3,6 +3,7 @@ package edu.android.gitproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
     }
 }
